5739a9a74ab2c71b4e9dbc56b29b475b438218ec
Razafinjatovo Monier Michel <monier_michel@yahoo.fr>
2021-12-20 11:44:30 +0100

    chore: Add ProductService "blank" project scoping React and Asp.net core

---
+++ ProductService/VeilleConcurrentielle.ProductService.WebApp/ClientApp/src/components/footer.jsx
@@ -0,0 +1,9 @@
+﻿const Footer = () => {
+    return (
+        <div className="text-center">
+            Monier, Issoufou 2021 (c)
+        </div>
+    )
+}
+
+export { Footer };
---
+++ ProductService/VeilleConcurrentielle.ProductService.WebApp/ClientApp/src/components/header.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import {
+    AppBar,
+    Box,
+    IconButton,
+    Toolbar,
+    Typography
+} from "@mui/material";
+import { Menu } from "@mui/icons-material";
+
+const Header = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                        <Menu />
+                    </IconButton>
+                    <Typography variant="h6" color="inherit" component="div">
+                        Catalogue
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+
+export { Header };
